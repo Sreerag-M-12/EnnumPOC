2684aee6b4bec4a7c87473bcbe90fb7b3df3c09a
Sreerag Rajesh Babu <Sreerag.Babu@asl.mum.sst>
2024-07-16 19:45:20 +0530

    init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnnumPOC.Types;
+
+namespace EnnumPOC
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int myNum = (int)Months.april;
+            Console.WriteLine(myNum);
+
+
+            Console.WriteLine("Months");
+            foreach (string name in Enum.GetNames(typeof(Months)))
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+    
+}
---
+++ Types/Months.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnnumPOC.Types
+{
+    
+        enum Months
+        {
+            january = 1,      //set as first month of the year  
+            february,
+            march,
+            april,
+            may,
+            june,
+            july,
+            august,
+            september,
+            october,
+            november,
+            december
+        }
+
+    
+}
